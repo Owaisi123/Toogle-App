e547b743b899b2cbc75a7aa361888a81f743cb3f
Muhammad Owais <165272983+Owaisi123@users.noreply.github.com>
2025-04-30 21:37:40 +0500

    Update Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React , {useContext} from 'react';
+import { ThemeContext } from '../Context/ThemeContext';
+
 
 const Header = () => {
+      const { theme } = useContext(ThemeContext);
+  
   return (
-    <div className="bg-black text-white py-6 px-4">
+    <div className={ ` ${theme == 'light' ? 'bg-white text-black' : 'bg-black text-white' } py-6 px-4`}>
       <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto gap-4">
-        <h1 className="text-3xl font-bold">Toogle</h1>
+        <h1 className="text-3xl font-bold">Owais</h1>
 
         <ul className="flex flex-col md:flex-row gap-4 md:gap-10 items-center">
           <li className="cursor-pointer">Home</li>
