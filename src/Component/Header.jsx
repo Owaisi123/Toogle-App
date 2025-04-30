b507ffc268f60894144b31dfedf2e33773d053b4
Muhammad Owais <165272983+Owaisi123@users.noreply.github.com>
2025-04-30 20:07:31 +0500

    Update Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="bg-black text-white py-6 px-4">
       <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto gap-4">
-        <h1 className="text-3xl font-bold">Owais</h1>
+        <h1 className="text-3xl font-bold">Toogle</h1>
 
         <ul className="flex flex-col md:flex-row gap-4 md:gap-10 items-center">
           <li className="cursor-pointer">Home</li>
