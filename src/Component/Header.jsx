22e74cd038525e2435a8c3c11bacd06e05e581a6
Muhammad Owais <165272983+Owaisi123@users.noreply.github.com>
2025-04-30 18:56:20 +0500

    Update Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='flex justify-around items-center bg-black text-white py-6'>
-      <h1 className='text-3xl font-bold'>Owais</h1>
-      <ul className='flex gap-10 cursor-pointer'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <button className='bg-slate-300 px-4 py-2 rounded-sm text-black'>
-        Sign Up
-      </button>
+    <div className="bg-black text-white py-6 px-4">
+      <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto gap-4">
+        <h1 className="text-3xl font-bold">Owais</h1>
+
+        <ul className="flex flex-col md:flex-row gap-4 md:gap-10 items-center">
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">About</li>
+          <li className="cursor-pointer">Contact</li>
+        </ul>
+
+        <button className="bg-slate-300 px-4 py-2 rounded-sm text-black">
+          Sign Up
+        </button>
+      </div>
     </div>
   );
 };
