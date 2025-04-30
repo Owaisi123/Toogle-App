7409c888ab74fe7fc19b172247f44a14bbf867dd
Muhammad Owais <165272983+Owaisi123@users.noreply.github.com>
2025-04-30 21:36:45 +0500

    Update Footer.jsx

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React , {useContext} from 'react';
+import { ThemeContext } from '../Context/ThemeContext';
+
 
 const Footer = () => {
+    const { theme } = useContext(ThemeContext);
+  
   return (
-    <footer className="bg-black text-white px-6 py-12 mt-24">
+    <footer className={`${theme == 'light' ? 'bg-white text-black' : 'bg-black text-white'} px-6 py-12 mt-24`}>
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
-        <h2 className="text-2xl font-bold">Toogle App</h2>
+        <h2 className="text-2xl font-bold">Owais</h2>
 
-        <ul className="flex flex-col md:flex-row gap-4 text-gray-300 text-sm md:text-base">
+        <ul className={`${theme == 'light' ? 'text-black' : 'text-white'} flex flex-col md:flex-row gap-4 text-sm md:text-base`}>
           <li className="hover:text-white cursor-pointer">Home</li>
           <li className="hover:text-white cursor-pointer">About</li>
           <li className="hover:text-white cursor-pointer">Contact</li>
